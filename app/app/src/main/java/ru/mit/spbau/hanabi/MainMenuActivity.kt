9f698095ab0388353b0d62a2900198545fb5eca4
equivalence1 <equivalence1@gmail.com>
2018-04-01 16:13:25 +0300

    Added stubs for the following activities: Single player Multi player Rules Settings

--- app/app/src/main/java/ru/mit/spbau/hanabi/MainMenuActivity.kt
+++ app/app/src/main/java/ru/mit/spbau/hanabi/MainMenuActivity.kt
@@ -1,13 +1,42 @@
 package ru.mit.spbau.hanabi
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.Button
 
 class MainMenuActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_menu)
+
+        val singlePlayerButton = findViewById<Button>(R.id.single_player_button)
+        singlePlayerButton?.setOnClickListener { _ ->
+            val intent = Intent(this, SinglePlayerActivity::class.java)
+            this.startActivity(intent)
+        }
+
+        val multiPlayerButton = findViewById<Button>(R.id.multi_player_button)
+        multiPlayerButton?.setOnClickListener { _ ->
+            val intent = Intent(this, MultiPlayerActivity::class.java)
+            this.startActivity(intent)
+        }
+
+        val rulesButton = findViewById<Button>(R.id.rules_button)
+        rulesButton?.setOnClickListener { _ ->
+            val intent = Intent(this, RulesActivity::class.java)
+            this.startActivity(intent)
+        }
+
+        val settingsButton = findViewById<Button>(R.id.setting_button)
+        settingsButton?.setOnClickListener { _ ->
+            val intent = Intent(this, SettingsActivity::class.java)
+            this.startActivity(intent)
+        }
+
     }
+
+
 
 }
---
+++ app/app/src/main/java/ru/mit/spbau/hanabi/MultiPlayerActivity.kt
@@ -0,0 +1,13 @@
+package ru.mit.spbau.hanabi
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class MultiPlayerActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_multi_player)
+    }
+
+}
