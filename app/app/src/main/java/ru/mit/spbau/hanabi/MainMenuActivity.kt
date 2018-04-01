566986dc73da1ad99656d061d422725b06b0f94f
equivalence1 <equivalence1@gmail.com>
2018-04-01 11:42:06 +0300

    Init android project

---
+++ app/app/src/main/java/ru/mit/spbau/hanabi/MainMenuActivity.kt
@@ -0,0 +1,13 @@
+package ru.mit.spbau.hanabi
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class MainMenuActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_menu)
+    }
+
+}
