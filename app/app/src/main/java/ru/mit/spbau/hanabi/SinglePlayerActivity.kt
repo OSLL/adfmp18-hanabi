7bccde6e61b8284fdc1815a349becb1bc9084a4c
equivalence1 <equivalence1@gmail.com>
2018-04-17 13:18:59 +0300

    Single player base ui stabs added

--- app/app/src/main/java/ru/mit/spbau/hanabi/SinglePlayerActivity.kt
+++ app/app/src/main/java/ru/mit/spbau/hanabi/SinglePlayerActivity.kt
@@ -2,11 +2,21 @@ package ru.mit.spbau.hanabi
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.NumberPicker
 
 class SinglePlayerActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_single_player)
+        setupNumberPicker()
     }
+
+    private fun setupNumberPicker() {
+        val nPicker = findViewById<NumberPicker>(R.id.players_number_picker)
+        nPicker.minValue = 2
+        nPicker.maxValue = 5
+        nPicker.wrapSelectorWheel = false
+    }
+
 }
